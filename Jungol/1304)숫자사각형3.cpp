729ff0d54c048e65556c44b130f4316373a9204f
Changmin Kim <41724458+KimRabbert@users.noreply.github.com>
2026-08-28 14:28:38 +0900

    Solved Jungol_1304

---
+++ Jungol/1304)숫자사각형3.cpp
@@ -0,0 +1,19 @@
+// Jungol 1304 숫자사각형3
+// 2026-08-28
+// Solved by cmKim
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << i + j * n + 1 << ' ';
+		}
+		cout << '\n';
+	}
+}
