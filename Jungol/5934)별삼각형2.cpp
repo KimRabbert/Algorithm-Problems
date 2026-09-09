78610aa2708048f861e9e48a459f0f9f38b756a5
Changmin Kim <41724458+KimRabbert@users.noreply.github.com>
2026-09-10 02:29:51 +0900

    Solved Jungol_5934

---
+++ Jungol/5934)별삼각형2.cpp
@@ -0,0 +1,33 @@
+// Jungol 5934 별삼각형2
+// 2026-09-10
+// Solved by cmKim
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int size;
+
+	cin >> size;
+
+	if (size > 100 || size < 1 || size % 2 == 0) {
+		cout << "INPUT ERROR!";
+		return 0;
+	}
+
+	for (int i = 0; i < size / 2; i++) {
+		for (int j = 0; j < i; j++)
+			cout << ' ';
+		for (int j = 0; j < size / 2 - i + 1; j++)
+			cout << '*';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < size / 2 + 1; i++) {
+		for (int j = 0; j < size / 2; j++)
+			cout << ' ';
+		for (int j = 0; j < i + 1; j++)
+			cout << '*';
+		cout << '\n';
+	}
+}
