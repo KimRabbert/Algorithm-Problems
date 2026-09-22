97003d0457347aca1edccd9048d0d90f95803c0f
Changmin Kim <41724458+KimRabbert@users.noreply.github.com>
2026-09-22 17:21:54 +0900

    Create 1157)버블정렬.cpp

---
+++ Jungol/1157)버블정렬.cpp
@@ -0,0 +1,27 @@
+// Jungol 1157 버블정렬
+// 2026-09-22
+// Solved by cmKim
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	int arr[100];
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+		}
+
+		for (int j = 0; j < n; j++)
+			cout << arr[j] << ' ';
+		cout << '\n';
+	}
+}
