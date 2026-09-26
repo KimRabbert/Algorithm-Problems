dd091d116225b9c70ee75182645c95ee5937fd72
Changmin Kim <41724458+KimRabbert@users.noreply.github.com>
2026-09-27 00:34:32 +0900

    Create 5917)접시 닦기.cpp

---
+++ Jungol/5917)접시 닦기.cpp
@@ -0,0 +1,26 @@
+// Jungol 5917 접시 닦기
+// 2026-09-26
+// Solved by cmKim
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int n, t, c, d;
+	stack <int> space[3];
+
+	cin >> n >> t;
+
+	for (int i = n; i > 0; i--)
+		space[0].push(i);
+
+	for (int i = 0; i < t; i++) {
+		cin >> c >> d;
+
+		for (int j = 0; j < d; j++) {
+			space[c].push(space[c - 1].top());
+			space[c - 1].pop();
+		}
+	}
+}
